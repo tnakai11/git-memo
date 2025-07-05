04a9178bdb153230b07d660a4a4e1580477a582e
Takumi Nakai <44494152+tnakai11@users.noreply.github.com>
2025-07-05 23:34:28 +0900

    Add Clap dependency and placeholder CLI

--- tests/cli.rs
+++ tests/cli.rs
@@ -2,9 +2,10 @@ use assert_cmd::Command;
 use predicates::prelude::*;
 
 #[test]
-fn runs() {
+fn shows_help() {
     let mut cmd = Command::cargo_bin("git-memo").unwrap();
-    cmd.assert()
+    cmd.arg("--help")
+        .assert()
         .success()
-        .stdout(predicate::str::contains("git-memo"));
+        .stdout(predicate::str::contains("add"));
 }
