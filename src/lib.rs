c26367dd1dc51be9d0426edd1fb6cd69dcf6efd6
tnakai11 <>
2025-07-19 11:07:59 +0900

    feat: add grep command

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 pub mod commands;
 
 pub use commands::{
-    add_memo, archive_category, edit_memo, list_categories, list_memos, remove_memos,
+    add_memo, archive_category, edit_memo, grep_memos, list_categories, list_memos, remove_memos,
 };
